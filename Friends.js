29470092f3b4edce73b6967e3adba209e32ef9e4
KatAlquijay <47570333+KatAlquijay@users.noreply.github.com>
2019-08-02 10:23:23 -0400

    Add files via upload

---
+++ Friends.js
@@ -0,0 +1,17 @@
+var friends = [
+    {
+    name: "Konata",
+    photo: "https://giphy.com/gifs/girl-kawaii-hbDvSMlXOf1uM"
+    scores: [
+            '3',
+            '2',
+            '5',
+            '5',
+            '4',
+            '1',
+            '2',
+            '3',
+            '3',
+            '1',
+    ]
+},
